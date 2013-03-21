765bbb8b3c382006f76f26a51361637d65e15bb5
Diego Elio Pettenò <flameeyes@flameeyes.eu>
2013-03-21 15:21:18 -0700

    Fix variable expansion.

--- manifests/variable.pp
+++ manifests/variable.pp
@@ -5,13 +5,13 @@ define env::variable($content) {
 
   case $::osfamily {
     'RedHat': {
-      concat::fragment { "env_var_$name_redhat_sh":
+      concat::fragment { "env_var_${name}_redhat_sh":
         target => '/etc/profile.d/puppet.sh',
         content => "export ${name}=${content}\n",
         order => 01,
       }
 
-      concat::fragment { "env_var_$name_redhat_csh":
+      concat::fragment { "env_var_${name}_redhat_csh":
         target => '/etc/profile.d/puppet.csh',
         content => "setenv ${name} ${content}\n",
         order => 01,
@@ -20,7 +20,7 @@ define env::variable($content) {
     default: {
       case $::operatingsystem {
         'Gentoo': {
-          concat::fragment { "env_var_$name_gentoo":
+          concat::fragment { "env_var_${name}_gentoo":
             target => "/etc/env.d/99puppet",
             content => "${name}=${content}\n",
             order => 01,
